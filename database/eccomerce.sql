2e87d7231d250523b29ae28fae76812f2f7637ac
sahil <70463282+returnofsahil@users.noreply.github.com>
2022-06-24 09:47:20 +0530

    Update eccomerce.sql

--- database/eccomerce.sql
+++ database/eccomerce.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 20, 2020 at 11:01 AM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.1.33
+-- Generation Time: Jun 24, 2022 at 06:13 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `shopee`
+-- Database: `ecommerce`
 --
 
 -- --------------------------------------------------------
@@ -33,6 +32,13 @@ CREATE TABLE `cart` (
   `user_id` int(11) NOT NULL,
   `item_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`cart_id`, `user_id`, `item_id`) VALUES
+(33, 1, 3);
 
 -- --------------------------------------------------------
 
@@ -54,15 +60,15 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`item_id`, `item_brand`, `item_name`, `item_price`, `item_image`, `item_register`) VALUES
-(1, 'Raymond', 'King Sized Bedesheets', 2000, './assets/bed1.jpeg', '2020-03-28 11:08:57'), -- NOW()
-(2, 'Decor', 'King Sized Bedesheets', 2200, './assets/bed2.jpeg', '2020-03-28 11:08:57'),
-(3, 'Bombay Dying', 'King Sized Bedesheets', 2000, './assets/bed3.jpeg', '2020-03-28 11:08:57'),
-(4, 'PRV', 'King Sized Bedesheets', 2000, './assets/bed4.jpeg', '2020-03-28 11:08:57'),
-(5, 'PRV', 'King Sized Bedesheets', 2000, './assets/bed5.jpeg', '2020-03-28 11:08:57'),
-(6, 'PRV', 'Necklase',1000 , './assets/jewel3.jpeg', '2020-03-28 11:08:57'),
-(7, 'PRV', 'Ring', 120, './assets/jewel4.jpeg', '2020-03-28 11:08:57'),
-(8, 'PRV', 'Suits', 600.00, './assets/suits.jpeg', '2020-03-28 11:08:57');
-
+(1, 'bedsheets', 'King Sized Bedesheets', 2000.00, './assets/bed1.jpeg', '2020-03-28 11:08:57'),
+(2, 'bedsheets', 'King Sized Bedesheets', 2200.00, './assets/bed2.jpeg', '2020-03-28 11:08:57'),
+(3, 'bedsheets', 'King Sized Bedesheets', 2000.00, './assets/bed3.jpeg', '2020-03-28 11:08:57'),
+(4, 'bedsheets', 'King Sized Bedesheets', 2000.00, './assets/bed4.jpeg', '2020-03-28 11:08:57'),
+(5, 'bedsheets', 'King Sized Bedesheets', 2000.00, './assets/bed5.jpeg', '2020-03-28 11:08:57'),
+(6, 'jewels', 'Necklase', 1000.00, './assets/jewel3.jpeg', '2020-03-28 11:08:57'),
+(14, 'suits', 'Suits', 700.00, './assets/sui6.jpeg', NULL),
+(15, 'nightsuits', 'Sweet Touch Nightsuits', 800.00, './assets/night5.jpeg', '2022-06-15 10:53:37'),
+(19, 'bedsheets', 'cotton', 2000.00, '../assets/62adef37b1ce67.96674625.jpg', NULL);
 
 -- --------------------------------------------------------
 
@@ -72,8 +78,8 @@ INSERT INTO `product` (`item_id`, `item_brand`, `item_name`, `item_price`, `item
 
 CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
   `register_date` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -81,21 +87,10 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `register_date`) VALUES
-(1, 'Aryan', 'Bhandari', '2020-03-28 13:07:17'),
-(2, 'Sahil', 'Bhandari', '2020-03-28 13:07:17');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `wishlist`
---
-
-CREATE TABLE `wishlist` (
-  `cart_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `user` (`user_id`, `username`, `password`, `register_date`) VALUES
+(1, 'sahil', 'bhandari', '2020-03-28 13:07:17'),
+(6, 'aryan', '$2y$10$bGQiOVKq5Bho.LnrxeGXZubcOk/SYFn8DOe4XV9REspbRYsZOt4Ku', NULL),
+(7, 'poonam', '$2y$10$uqKwkzDkGZpN0zQDaLVo4exx/gDbb5Symlhorzbh/ntSRixCbUZTC', NULL);
 
 --
 -- Indexes for dumped tables
@@ -127,19 +122,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
